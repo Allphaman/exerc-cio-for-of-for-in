675526e99678609ce698fc2dd37686a3b1333c5c
Allphaman <Cleitoncampos>
2019-06-11 17:02:26 -0300

    adicionar exercício for of e for in

---
+++ pagina.js
@@ -0,0 +1,63 @@
+
+// let media = Number(prompt("Digite sua média:"));
+
+// do {
+//     media = prompt("Digite sua média");
+
+//     if (media == null && media ==  ""){
+
+//         media = prompt("Digite sua média:")
+//     }
+
+// }
+// while (media == null || media == "");
+
+//     media = prompt( "Digite sua média");
+
+// if (Media >10 || media  <0){
+//     alert("Nota invalida")
+// }
+
+// else if(media >=7) {
+//     alert ("Aprovado")
+// }
+
+// else if (media < 7 && media >=5){
+
+//     alert("Exame");
+// }
+
+// else {
+//     alert("Reprovado");
+// }
+
+
+
+// let nome = "lucas";
+// let contador = 0;
+
+// while (contador <4){
+//     console.log (nome);
+
+//     contador ++;
+// }
+
+// for (let i = 0; i < 10000; i++){
+
+//     console.log("Cleiton");
+// }
+
+let signos = [" aries","   aquario"," cancer","peixes"];
+
+
+
+// for(let i = 0; i<signos.length; i++){
+//     // iterar listar = contar o total de lista
+//     console.log(signos[i]);
+// }
+
+
+for (indice in signos){
+
+    console.log("O signo no indice" + indice + "é" + [indice]);
+}
